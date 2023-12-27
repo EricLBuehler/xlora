66f5b7969a4ad2fa08f9877874bc638d7dbc2a0c
EricLBuehler <ericlbuehler@gmail.com>
2023-12-27 15:48:56 -0500

    Add infrastructure for inital bound method swapping

---
+++ src/mole.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+from peft.tuners import lora
+
+from mole.mole_insertion_layers import MoLELinear
+
+def convert_layers_to_mole(base: nn.Module):
+    modules = list(base.modules())
+    for module in modules:
+        if isinstance(module, lora.Linear):
+            new_layer = MoLELinear(module)
+            module.forward = new_layer.forward
