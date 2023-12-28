f30a1bc3607a921533d7574acc96555b7e898a25
EricLBuehler <ericlbuehler@gmail.com>
2023-12-28 13:17:31 -0500

    Rework MoLELinear, add TODOs

--- src/mole.py
+++ src/mole.py
@@ -9,5 +9,7 @@ def convert_layers_to_mole(base: nn.Module):
     modules = list(base.modules())
     for module in modules:
         if isinstance(module, lora.Linear):
-            new_layer = MoLELinear(module)
+            new_layer = MoLELinear(
+                [module]
+            )  # TODO(EricLBuehler): Use the passed LoRA adapters for this layer
             module.forward = new_layer.forward
