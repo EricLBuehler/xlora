442fd7d999592ea057971c6d461761700217ee41
EricLBuehler <ericlbuehler@gmail.com>
2023-12-28 13:17:31 -0500

    Rework MoLELinear, add TODOs

--- src/mole/mole_insertion_layers.py
+++ src/mole/mole_insertion_layers.py
@@ -1,38 +1,19 @@
+from ast import List
 from typing import Any
 from peft.tuners import lora
 from torch import Tensor
 
-from utils.mole_utils import is_scalar_tensor
+from mole import mole_state
 
 
-class MoLELinear(lora.Linear):
-    def __init__(self, other: lora.Linear) -> None:
-        for attr in dir(other):
-            if not hasattr(self, attr):
-                setattr(self, attr, getattr(other, attr))
+class MoLELinear:
+    def __init__(self, adapters: List[lora.Linear]) -> None:
+        # TODO(EricLBuehler): Freeze the LoRA adapters
+        for adapter in adapters:
+            pass
+        self.adapters = adapters
 
-    def forward(self, x: Tensor, scaling: Tensor, *args: Any, **kwargs: Any) -> Tensor:
-        assert is_scalar_tensor(scaling)
-
-        previous_dtype = x.dtype
-
-        if self.disable_adapters:
-            if self.merged:
-                self.unmerge()
-            result = self.base_layer(x, *args, **kwargs)
-        elif self.merged:
-            result = self.base_layer(x, *args, **kwargs)
-        else:
-            result = self.base_layer(x, *args, **kwargs)
-            for active_adapter in self.active_adapters:
-                if active_adapter not in self.lora_A.keys():
-                    continue
-                lora_A = self.lora_A[active_adapter]
-                lora_B = self.lora_B[active_adapter]
-                dropout = self.lora_dropout[active_adapter]
-                # scaling = self.scaling[active_adapter] # This is the only change
-                x = x.to(lora_A.weight.dtype)
-                result += lora_B(lora_A(dropout(x))) * scaling
-
-        result = result.to(previous_dtype)
-        return result
+    def forward(self, x: Tensor, *args: Any, **kwargs: Any) -> Tensor:
+        # TODO(EricLBuehler): Combine the LoRA adapters using the scaling
+        _scaling = mole_state.get_scalings()
+        pass
--- src/utils/mole_utils.py
+++
@@ -1,5 +0,0 @@
-from torch import Tensor
-
-
-def is_scalar_tensor(tensor: Tensor) -> bool:
-    return tensor.ndim == 0
