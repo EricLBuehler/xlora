92fb683fc50926a51fc3404297fc8ba9c147494b
EricLBuehler <ericlbuehler@gmail.com>
2023-12-29 19:43:55 -0500

    Fix more imports

--- src/mole/mole/mole.py
+++ src/mole/mole/mole.py
@@ -6,7 +6,7 @@ from peft.tuners.tuners_utils import PeftConfig
 
 from mole.mole import mole_state
 from mole.mole.mole_classifier import MoLEClassifier, MoLEClassifierConfig
-from mole.mole_insertion_layers import MoLELayer
+from mole.mole.mole_insertion_layers import MoLELayer
 
 
 def convert_layers_to_mole(
