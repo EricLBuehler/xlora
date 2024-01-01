a13312e5ea5c55c45afefa9379ee3bf641f1e002
EricLBuehler <ericlbuehler@gmail.com>
2023-12-31 21:10:59 -0500

    Expose more methods

--- src/mole/__init__.py
+++ src/mole/__init__.py
@@ -1,3 +1,3 @@
 from .mole import mole_state
-from .mole.mole import add_mole_to_model
+from .mole.mole import add_mole_to_model, get_nb_trainable_parameters, print_trainable_parameters
 from .mole.mole_config import MoLEConfig
