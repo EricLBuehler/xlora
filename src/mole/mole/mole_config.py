a0fab8ba65fa5c4089db61f2df039dd09e20cff1
EricLBuehler <ericlbuehler@gmail.com>
2024-01-01 07:11:38 -0500

    Fix config hidden size

--- src/mole/mole/mole_config.py
+++ src/mole/mole/mole_config.py
@@ -1,12 +1,15 @@
+from typing import Optional
+
+
 class MoLEConfig:
     r"""
     This is the configuration class to store the configuration of a [`MoLEClassifier`].
 
     Args:
+        hidden_size (`int`):
+            Dimension of the hidden representations for the base model.
         top_k_lora (`int`, *optional*, defaults to None):
             Sparesely select the top_k LoRA experts instead of the default dense method.
-        hidden_size (`int`, *optional*, defaults to 4096):
-            Dimension of the hidden representations.
         pad_token_id (`int`, *optional*):
             The id of the padding token.
 
@@ -16,9 +19,9 @@ class MoLEConfig:
 
     def __init__(
         self,
-        top_k_lora=None,
-        hidden_size=4096,
-        pad_token_id=None,
+        hidden_size: int,
+        top_k_lora: Optional[int] = None,
+        pad_token_id: Optional[int] = None,
     ):
         self.top_k_lora = top_k_lora
 
