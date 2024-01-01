5646969f1ea9a4908731d3ec125b66ef41a3b8b4
EricLBuehler <ericlbuehler@gmail.com>
2023-12-31 21:34:52 -0500

    Expand API

--- src/mole/mole/mole_state.py
+++ src/mole/mole/mole_state.py
@@ -1,10 +1,37 @@
-from typing import Optional
+from typing import Optional, override
 
 from torch import Tensor
 
 from mole.mole.mole_classifier import MoLEClassifier
 
-_scalings: Optional[Tensor] = None
+
+class _MoLEScalings:
+    def __init__(self, value: Tensor) -> None:
+        self.value = value
+
+    @property
+    def value(self) -> Tensor:
+        return self.value
+
+
+class _MoLEScalingsWithLifetime(_MoLEScalings):
+    def __init__(self, value: Tensor, old_scalings: Tensor, n_accesses_lifetime: int) -> None:
+        super().__init__(value)
+        self.old_scalings = old_scalings
+        self.n_accesses_lifetime = n_accesses_lifetime
+        self.n_accesses = 0
+
+    @override
+    @property
+    def value(self) -> Tensor:
+        self.n_accesses += 1
+        result = super().value
+        if self.n_accesses >= self.n_accesses_lifetime:
+            self.value = self.old_value
+        return result
+
+
+_scalings: Optional[_MoLEScalings] = None
 
 
 def get_scalings() -> Tensor:
@@ -12,15 +39,28 @@ def get_scalings() -> Tensor:
     Reads the scaling states (a tensor with 2 dims), raising AssertionError if the state has not been set.
     """
     assert _scalings is not None
-    return _scalings
+    return _scalings.value
 
 
 def set_scalings(value: Tensor) -> None:
     """
-    Sets the scaling states to a Tensor. A tensor with 2 dim is expected: (batch_size, num_classes)
+    Sets the scaling states to a Tensor.
+
+    A tensor with 2 dim is expected: (batch_size, num_classes)
     """
     assert value.ndim == 2
-    _scalings = value
+    _scalings = _MoLEScalings(value)
+
+
+def set_scalings_lifetime(value: Tensor, n_accesses_lifetime: int) -> None:
+    global _scalings
+    """
+    Sets the scaling states to a Tensor. The scaling states will have a lifetime of n accesses.
+
+    A tensor with 2 dim is expected: (batch_size, num_classes)
+    """
+    assert value.ndim == 2
+    _scalings = _MoLEScalingsWithLifetime(value, _scalings.value, n_accesses_lifetime)
 
 
 _mole_classifier: Optional[MoLEClassifier] = None
