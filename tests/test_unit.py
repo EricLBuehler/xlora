45b6744faaa3361fa09c5468e91d15e3a0dc4960
EricLBuehler <ericlbuehler@gmail.com>
2024-01-02 10:29:44 -0500

    Add pytest

---
+++ tests/test_unit.py
@@ -0,0 +1,25 @@
+import mole
+
+
+def test_items():
+    assert dir(mole) == [
+        "MoLEConfig",
+        "__builtins__",
+        "__cached__",
+        "__doc__",
+        "__file__",
+        "__loader__",
+        "__name__",
+        "__package__",
+        "__path__",
+        "__spec__",
+        "add_mole_to_model",
+        "get_nb_trainable_parameters",
+        "mole",
+        "mole_classifier",
+        "mole_config",
+        "mole_insertion_layers",
+        "mole_state",
+        "print_trainable_parameters",
+        "set_scalings_with_lifetime",
+    ]
