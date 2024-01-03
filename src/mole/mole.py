8c98453c629961101269a2f900187b8d3e10ac47
EricLBuehler <ericlbuehler@gmail.com>
2024-01-03 05:14:38 -0500

    Improve from_pretrained

--- src/mole/mole.py
+++ src/mole/mole.py
@@ -56,7 +56,7 @@ def add_mole_to_model(
     svd_driver: Optional[str] = None,
 ) -> MoLEModel:
     """
-    This method converts all LoRA adapters to MoLE layers, and it is the intended entrypoint
+    This method converts all LoRA adapters to MoLE layers, and it is one of the intended entrypoints
     for use of MoLE. All LoRA adapters will be frozen, and the MoLEClassifier is initialized.
 
     When using the `cat` combination_type you should be aware that rank of the resulting adapter will be equal to
@@ -85,7 +85,7 @@ def add_mole_to_model(
             one of [None, `gesvd`, `gesvdj`, `gesvda`]. For more info please refer to `torch.linalg.svd`
             documentation. Defaults to None.
     Returns:
-        model (`PeftMixedModel`):
+        model (`MoLEModel`):
             The new model.
     """
 
@@ -136,16 +136,61 @@ def add_mole_to_model(
     return MoLEModel(model)
 
 
-def from_pretrained(load_directory: str, from_safetensors: bool):
+def from_pretrained(
+    load_directory: str,
+    from_safetensors: bool,
+    model: PreTrainedModel,
+    mole_config: MoLEConfig,
+    adapters: Dict[str, str],
+    combination_type: str = "svd",
+    svd_rank: Optional[bool] = None,
+    svd_clamp: Optional[float] = None,
+    svd_full_matrices: Optional[bool] = True,
+    svd_driver: Optional[str] = None,
+) -> MoLEModel:
     """
-    Loads a pretrained classifier from the specified folder. This is the counterpart to `MoLEModel.save_pretrained`.
+    Loads a pretrained classifier from the specified folder while initializing the model. This is the counterpart to `MoLEModel.save_pretrained`.
+
+    This method is very similar to `add_mole_to_model`: it converts all LoRA adapters to MoLE layers, and it is one of
+    the intended entrypoints for use of MoLE. All LoRA adapters will be frozen, and the MoLEClassifier is initialized.
+
+    When using the `cat` combination_type you should be aware that rank of the resulting adapter will be equal to
+    the sum of all adapters ranks. So it's possible that the mixed adapter may become too big and result in OOM
+    errors.
 
     Args:
         load_directory (`str`):
             The directory to load the classifier weights from.
         from_safetensors (`bool`):
             Whether to load the classifier weights from a .pt or .safetensors file.
+        model (`PreTrainedModel`):
+            The model to add the LoRA adapters to. It may be modified in place.
+        adapters (`dict`):
+            Mapping of adapter names to the LoRA adapter id, as per PeftModel.load_adapter. *They will be automatically loaded*, to use as LoRA experts.
+        combination_type (`str`):
+            Type of merging. Can be one of [`svd`, `linear`, `cat`]. When using the `cat` combination_type you
+            should be aware that rank of the resulting adapter will be equal to the sum of all adapters ranks. So
+            it's possible that the mixed adapter may become too big and result in OOM errors.
+        svd_rank (`int`, *optional*):
+            Rank of output adapter for svd. If None provided, will use max rank of merging adapters.
+        svd_clamp (`float`, *optional*):
+            A quantile threshold for clamping SVD decomposition output. If None is provided, do not perform
+            clamping. Defaults to None.
+        svd_full_matrices (`bool`, *optional*):
+            Controls whether to compute the full or reduced SVD, and consequently, the shape of the returned
+            tensors U and Vh. Defaults to True.
+        svd_driver (`str`, *optional*):
+            Name of the cuSOLVER method to be used. This keyword argument only works when merging on CUDA. Can be
+            one of [None, `gesvd`, `gesvdj`, `gesvda`]. For more info please refer to `torch.linalg.svd`
+            documentation. Defaults to None.
+    Returns:
+        model (`MoLEModel`):
+            The new model.
     """
+
+    model = add_mole_to_model(
+        model, mole_config, adapters, combination_type, svd_rank, svd_clamp, svd_full_matrices, svd_driver
+    )
 
     classifier = mole_state.get_mole_classifier()
     with open(os.path.join(load_directory, "mole_classifier_config.json"), "w") as f:
@@ -160,6 +205,8 @@ def from_pretrained(load_directory: str, from_safetensors: bool):
     else:
         state_dict = torch.load(os.path.join(load_directory, "mole_classifier.pt"))
     classifier.load_state_dict(state_dict)
+
+    return model
 
 
 def get_nb_trainable_parameters(model: PeftMixedModel) -> Tuple[int, int]:
