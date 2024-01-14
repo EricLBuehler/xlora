5159b1560b5fbc4134d95592f8b41cb582c79e43
EricLBuehler <ericlbuehler@gmail.com>
2024-01-14 14:25:49 -0500

    Update scalings logging API

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -16,6 +16,7 @@ def test_items():
         "add_mole_to_model",
         "disable_scalings_logging",
         "enable_scalings_logging",
+        "flush_log_scalings",
         "from_pretrained",
         "get_nb_trainable_parameters",
         "mole",
