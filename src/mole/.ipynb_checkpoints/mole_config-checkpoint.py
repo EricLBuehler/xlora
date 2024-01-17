4d5dda19b91f4bf6a42477e2c13139961a79a06d
Eric Buehler <ericlbuehler@gmail.com>
2024-01-16 19:47:55 -0500

    Our working change

---
+++ src/mole/.ipynb_checkpoints/mole_config-checkpoint.py
@@ -0,0 +1,50 @@
+from typing import Optional
+
+import torch
+
+
+class MoLEConfig:
+    r"""
+    This is the configuration class to store the configuration of a [`MoLEClassifier`].
+
+    Args:
+        hidden_size (`int`):
+            Hidden size of the base model.
+        device (`torch.device`):
+            Device for the MoLE classifier.
+        enable_softmax (`bool`, *optional*, defaults to `True`):
+            Enable softmax application for the MoLE classifier.
+        layerwise_scalings (`bool`, *optional*, defaults to `False`):
+            Generate scalings for each layer.
+        top_k_lora (`int`, *optional*, defaults to None):
+            Sparsely select the top_k LoRA experts instead of the default dense method.
+        mole_depth (`int`, *optional*, defaults to 1):
+            Depth of the MoLE classifier.
+        mole_size (`int`, *optional*, defaults to 32):
+            Hidden size of the MoLE classifier, irrelevant if `mole_depth=1`.
+        pad_token_id (`int`, *optional*):
+            The id of the padding token.
+
+    """
+
+    model_type = "mole"
+
+    def __init__(
+        self,
+        hidden_size: int,
+        device: torch.device,
+        enable_softmax: bool = True,
+        layerwise_scalings: bool = False,
+        top_k_lora: Optional[int] = None,
+        mole_depth: int = 1,
+        mole_size: int = 32,
+        pad_token_id: Optional[int] = None,
+    ):
+        self.device = device
+        self.enable_softmax = enable_softmax
+        self.top_k_lora = top_k_lora
+        self.hidden_size = hidden_size
+        self.pad_token_id = pad_token_id
+        self.mole_depth = mole_depth
+        self.mole_size = mole_size
+        self.layerwise_scalings = layerwise_scalings
