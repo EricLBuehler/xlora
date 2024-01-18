bd25de4f79ca231307401a11a4d0a1bb59e8bb1f
lamm-mit <mbuehler@MIT.EDU>
2024-01-18 15:58:37 -0500

    Fix circular imports

---
+++ src/xlora/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,9 @@
+from .xlora import (
+    add_xlora_to_model,
+    from_pretrained,
+    get_nb_trainable_parameters,
+    print_scalings_predictions,
+    print_trainable_parameters,
+    set_scalings_with_lifetime,
+)
+from .xlora_config import xLoRAConfig
