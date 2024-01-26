5f56d9359bc65ef04544461148b710d39ea1d11c
EricLBuehler <ericlbuehler@gmail.com>
2024-01-26 17:25:06 -0500

    Fix importerr

--- src/xlora/__init__.py
+++ src/xlora/__init__.py
@@ -1,12 +1,5 @@
 from .xlora import (
     add_xlora_to_model,
-    disable_scalings_logging,
-    enable_scalings_logging,
-    flush_log_scalings,
     from_pretrained,
-    get_nb_trainable_parameters,
-    print_scalings_predictions,
-    print_trainable_parameters,
-    set_scalings_with_lifetime,
 )
 from .xlora_config import xLoRAConfig
